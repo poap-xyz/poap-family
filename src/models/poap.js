bc0e082b9967edb485ca77b994eb670b51bcff29
Juan M <jm@guide42.com>
2023-05-31 11:22:30 +0200

    Add collector since date on profile

--- src/models/poap.js
+++ src/models/poap.js
@@ -11,11 +11,35 @@ function POAP(token) {
     owner: {
       id: typeof token.owner === 'object' ? token.owner.id : token.owner,
     },
+    created: !token.created ? undefined : new Date(token.created),
     event: !token.event ? undefined : Event(token.event),
   }
 }
 
 const POAP_SCAN_URL = 'https://app.poap.xyz/scan'
+
+function findInitialPOAPDate(tokens) {
+  return tokens.reduce(
+    (initialDate, token) => {
+      if (
+        token &&
+        token.created &&
+        token.created instanceof Date &&
+        (
+          initialDate === null ||
+          (
+            initialDate instanceof Date &&
+            initialDate > token.created
+          )
+        )
+      ) {
+        return token.created
+      }
+      return initialDate
+    },
+    null
+  )
+}
 
 export {
   POAP_FETCH_RETRIES,
@@ -23,4 +47,5 @@ export {
   POAP_API_KEY,
   POAP,
   POAP_SCAN_URL,
+  findInitialPOAPDate,
 }
