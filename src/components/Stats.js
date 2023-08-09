1ff732b541ade3f7a0b7dbafa57d6ef8bc762acb
Juan M <jm@guide42.com>
2023-08-09 13:06:39 +0200

    Move margin left on common stats to css

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -14,7 +14,6 @@ function Stats({ stats, highlight }) {
             style={{
               paddingLeft: highlight !== statName && index === 0 ? '.5rem' : undefined,
               paddingRight: highlight !== statName && index + 1 === entries.length ? '.5rem' : undefined,
-              marginLeft: highlight && highlight !== statName ? '.5rem' : undefined,
             }}
           >
             <div className="stat-content">
