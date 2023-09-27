0a53f990d078f10ae0ffc43f2b225e5abd2dcf8c
Juan M <jm@guide42.com>
2023-09-27 13:56:37 -0300

    Allow button link to be disabled

--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -1,8 +1,12 @@
 import '../styles/button-link.css'
 
-function ButtonLink({ title, onClick, className, children }) {
+function ButtonLink({ title, onClick, className, disabled, children }) {
   return (
-    <button className={`button-link${className ? ` ${className}` : ''}`} onClick={onClick}>
+    <button
+      className={`button-link${className ? ` ${className}` : ''}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
       <span title={title} className="button-link-content">{children}</span>
     </button>
   )
