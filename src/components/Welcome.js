123ab874056f599585e695ee8e38fa65f14628b2
Juan M <jm@guide42.com>
2024-02-09 20:05:16 -0300

    Add help page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -14,7 +14,14 @@ function Welcome() {
     <div className="welcome">
       <div className="welcome-content">
         Explore POAPs collections have in common.{' '}
-        <a className="link" href="https://poap.notion.site/POAP-Family-FAQ-cef29bc0bb8c4f8f936164d988a944cc" target="_blank" rel="noopener noreferrer">Here's how</a>.
+        <a
+          className="link"
+          href="https://poap.zendesk.com/hc/en-us/articles/24008770288909-How-to-navigate-POAP-Family"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Here's how
+        </a>.
         <br />
         <ButtonLink onClick={handleDismiss}>dismiss</ButtonLink>
       </div>
