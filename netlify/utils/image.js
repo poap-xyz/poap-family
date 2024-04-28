283288d40bfb71135afc294e553a344641199866
Juan M <jm@guide42.com>
2024-04-28 04:49:41 -0300

    Frame landscape

--- netlify/utils/image.js
+++ netlify/utils/image.js
@@ -3,16 +3,26 @@ export function renderEventsImages(events, canvas, size, pos) {
     return []
   }
 
+  let posX = 0
+  let posY = 0
+  if (typeof pos === 'number') {
+    posX = pos
+    posY = pos
+  } else if (Array.isArray(pos) && pos.length === 2) {
+    [posX, posY] = pos
+  }
+
   const angle = 360 / events.length
   const offset = size / 2
   const radius = (canvas - size) / 2
-  const center = pos + offset + radius
+  const centerX = posX + offset + radius
+  const centerY = posY + offset + radius
 
   const images = []
 
   events.forEach((event, i) => {
-    const x = center + radius * Math.cos(angle * i * Math.PI / 180)
-    const y = center + radius * Math.sin(angle * i * Math.PI / 180)
+    const x = centerX + radius * Math.cos(angle * i * Math.PI / 180)
+    const y = centerY + radius * Math.sin(angle * i * Math.PI / 180)
 
     images.push({
       type: 'img',
