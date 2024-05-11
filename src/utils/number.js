20aab7e265c19d9748d6583fac2fd79573c7381f
Juan M <jm@guide42.com>
2024-05-10 23:33:43 -0300

    Move get random int to utils

--- src/utils/number.js
+++ src/utils/number.js
@@ -21,3 +21,9 @@ export function getSearchParamNumber(searchParams, key, defaultValue) {
   }
   return defaultValue
 }
+
+export function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
