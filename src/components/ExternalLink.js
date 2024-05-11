69c40decf41dfc217319f3a43bc762a875086d72
Juan M <jm@guide42.com>
2024-05-10 22:56:36 -0300

    External link in help

--- src/components/ExternalLink.js
+++ src/components/ExternalLink.js
@@ -1,9 +1,14 @@
 import { OpenNewWindow } from 'iconoir-react'
 import '../styles/external-link.css'
 
-function ExternalLink({ href, children }) {
+function ExternalLink({ href, className, children }) {
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer" className="external-link">
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`external-link${className ? ` ${className}` : ''}`}
+    >
       {children ?? href}
       <OpenNewWindow className="external-link-icon" />
     </a>
