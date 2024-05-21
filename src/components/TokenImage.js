e04f775f73419dab91ae04308898a0f6ee64463b
Juan M <jm@guide42.com>
2024-05-21 09:03:08 -0300

    CSS adjustments

--- src/components/TokenImage.js
+++ src/components/TokenImage.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import { DropProps } from 'models/drop'
-import { getRandomInt } from 'utils/number'
-import 'styles/poap.css'
+import { resizeTokenImageUrl } from 'models/poap'
+import 'styles/token-image.css'
 
 /**
  * @param {PropTypes.InferProps<TokenImage.propTypes>} props
@@ -19,34 +19,7 @@ function TokenImage({
     : event.image_url
 
   if (resize) {
-    if (imgix) {
-      const url = new URL(imageUrl)
-      url.host = `poap${Math.trunc(getRandomInt(0, 10))}.imgix.net`
-      if (url.pathname.startsWith('/assets.poap.xyz')) {
-        url.pathname = url.pathname.substring('/assets.poap.xyz'.length)
-      }
-      if (size) {
-        imageUrl = url.toString() + `?w=${size}&h=${size}`
-      } else {
-        imageUrl = url.toString()
-      }
-    } else {
-      let poapSize = 'small'
-
-      if (size <= 64) {
-        poapSize = 'xsmall'
-      } else if (size > 64 && size <= 128) {
-        poapSize = 'small'
-      } else if (size > 128 && size <= 256) {
-        poapSize = 'medium'
-      } else if (size > 256 && size <= 512) {
-        poapSize = 'large'
-      } else if (size > 512) {
-        poapSize = 'xlarge'
-      }
-
-      imageUrl += `?size=${poapSize}`
-    }
+    imageUrl = resizeTokenImageUrl(imageUrl, size, imgix)
   }
 
   return (
@@ -54,8 +27,11 @@ function TokenImage({
       src={imageUrl}
       alt={event.name}
       title={event.name}
-      className="poap"
-      style={{ width: `${imgSize ?? size}px`, height: `${imgSize ?? size}px` }}
+      className="token-image"
+      style={{
+        width: `${imgSize ?? size}px`,
+        height: `${imgSize ?? size}px`,
+      }}
     />
   )
 }
